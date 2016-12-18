6bf203671bf487bfab1abe9f51e4194af0e62ad2
RohitvgMac <rohitvg@gmail.com>
2016-12-17 22:31:24 -0800

    Added the unsolved project for week1 assignment

---
+++ Week1/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,27 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(c: Int, r: Int): Int = ???
+  
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = ???
+  
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = ???
+  }
